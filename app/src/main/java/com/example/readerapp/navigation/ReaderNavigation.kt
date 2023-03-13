1e54de963dc47893ab9a9c8ff03896175a6ce563
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-13 15:20:11 +0300

    created ReaderNavigation.kt with NavHost and navController components

--- app/src/main/java/com/example/readerapp/navigation/ReaderNavigation.kt
+++ app/src/main/java/com/example/readerapp/navigation/ReaderNavigation.kt
@@ -1,1 +1,30 @@
 package com.example.readerapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.readerapp.screens.SplashScreen
+import com.example.readerapp.screens.home.HomeScreen
+import com.example.readerapp.screens.search.SearchScreen
+
+@Composable
+fun ReaderNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = ReaderScreens.SplashScreen.name,
+    ) {
+        composable(ReaderScreens.SplashScreen.name) {
+            SplashScreen(navController)
+        }
+
+        composable(ReaderScreens.ReaderHomeScreen.name) {
+            HomeScreen(navController)
+        }
+
+        composable(ReaderScreens.SearchScreen.name) {
+            SearchScreen(navController)
+        }
+    }
+}
