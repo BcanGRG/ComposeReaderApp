0371875048d364a515ae0bf5e23e7ed5de6d3e5d
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-13 15:19:09 +0300

    page edited according to ReaderNavigation

--- app/src/main/java/com/example/readerapp/MainActivity.kt
+++ app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.readerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.readerapp.navigation.ReaderNavigation
 import com.example.readerapp.ui.theme.ReaderAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,26 +22,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ReaderApp()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReaderApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) { ReaderNavigation() }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReaderAppTheme {
-        Greeting("Android")
     }
 }
