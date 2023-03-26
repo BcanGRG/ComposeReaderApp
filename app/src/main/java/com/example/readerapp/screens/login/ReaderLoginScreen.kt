d856c9f42f018ab1e378c2cd70b33207199f4211
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-03-26 22:52:45 +0300

    navigating to LoginScreen

--- app/src/main/java/com/example/readerapp/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/login/ReaderLoginScreen.kt
@@ -1,8 +1,10 @@
 package com.example.readerapp.screens.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 
 @Composable
-fun LoginScreen() {
-
+fun LoginScreen(navController: NavHostController) {
+    Text(text = "Login Screen!")
 }
